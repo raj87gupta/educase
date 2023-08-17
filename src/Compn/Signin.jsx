73bdd9631d8676c692238a905858b40fa2de781c
Rajesh Gupta <131549330+raj87gupta@users.noreply.github.com>
2023-08-17 11:00:01 +0530

    Signin.jsx

--- src/Compn/Signin.jsx
+++ src/Compn/Signin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "../Style/si.css";
-import {HashLink} from 'react-router-hash-link'
+import { HashLink } from "react-router-hash-link";
 const Login = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [hasValue, setHasValue] = useState(false);
@@ -25,44 +25,43 @@ const Login = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         </p>
       </div>
-      <form action="/account" method="submit">
-
-      <div
-        className={`custom-input ${isFocused ? "focus" : ""} ${
-          hasValue ? "has-value" : ""
-        }`}
-      >
-        <label className="input-label">Enter Address</label>
-        <input
-          type="text"
-          required
-          className="input-field"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onInput={handleInput}
-        />
-      </div>
-      <div
-        className={`custom-input ${isFocused ? "focus" : ""} ${
-          hasValue ? "has-value" : ""
-        }`}
-      >
-        <label className="input-label">Password</label>
-        <input
-          type="text"
-          required
-          className="input-field"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onInput={handleInput}
-        />
-      </div>
-      <div>
-        <button type="submit" className="button9">
-        <HashLink to={"/account"}>Login</HashLink>
-        </button>
-      </div>
-        </form>
+      <form action="/account" >
+        <div
+          className={`custom-input ${isFocused ? "focus" : ""} ${
+            hasValue ? "has-value" : ""
+          }`}
+        >
+          <label className="input-label">Enter Address</label>
+          <input
+            type="text"
+            required
+            className="input-field"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onInput={handleInput}
+          />
+        </div>
+        <div
+          className={`custom-input ${isFocused ? "focus" : ""} ${
+            hasValue ? "has-value" : ""
+          }`}
+        >
+          <label className="input-label">Password</label>
+          <input
+            type="text"
+            required
+            className="input-field"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onInput={handleInput}
+          />
+        </div>
+        <div>
+          <button type="submit" className="button9">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
